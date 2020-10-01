033c3a972444b0f78cd3a11e53b5d0535a361219
Malik_Kaleem <malikkaleem171@gmail.com>
2020-10-01 17:25:15 +0500

    Updates

---
+++ precache-manifest.4231dc343193d5d65f914e324db727d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "098c1e88117bb249b2ad4576e60a3513",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c61559573d7294d6331c",
+    "url": "/static/css/main.24844bd8.chunk.css"
+  },
+  {
+    "revision": "0b9ddfd7c32ddb23fdb7",
+    "url": "/static/js/2.c08d05ff.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c08d05ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c61559573d7294d6331c",
+    "url": "/static/js/main.3b7c6dec.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  }
+]);
